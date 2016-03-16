9a1baf4ff1fc2ed96ddb6d44b32a7db7d027ff7b
purpleCrow <408448115@qq.com>
2016-03-16 12:45:03 +0800

    ceshi scrolView

---
+++ 02--UIScrolView/02--UIScrolView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02--UIScrolView
+//
+//  Created by 千锋 on 16/2/29.
+//  Copyright (c) 2016年 千锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
